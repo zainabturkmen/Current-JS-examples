7c35e90bcd56e6edea196fc1063da1ba640f1578
Zainab <zainabturkmen247@gmail.com>
2023-06-13 18:13:11 +0430

    init

--- script.js
+++ script.js
@@ -250,6 +250,7 @@ btn.forEach(button => {
     button.addEventListener('click', ()=> {
         console.log('click hehehe')
     })
+    
 });
 
 const GrandPa = document.getElementById('GrandParent')
