13e7b1642f8bb2a6cf457c49b7a96939b8c978f2
Zainab <zainabturkmen247@gmail.com>
2023-12-04 16:52:00 +0430

    init

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 
 
 
-// function myName(name, callback){
-//     callback ('hello '  + name)
-// }
-
-// function namei(variable){
-//     console.log(variable)
-// }
-// myName('zainab', namei)
-
 function numbers(a, b){
       a + b
 }
-
 
 function result(sum){
     sum(1 , 5)
