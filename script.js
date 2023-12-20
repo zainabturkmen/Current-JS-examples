1f65f2b96d752a42b4504804c549676bb8d6d890
Zainab <zainabturkmen247@gmail.com>
2023-12-20 16:28:50 +0430

    init

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+// Arrow function with one parameter 
+
 let namei = name => {
     return `Ya ${name}`;
 }
 
 console.log(namei('Rabbim'))
+
+// Arrow function with tow parameter
+
+let towParameter = (HELLO, NAME) => {
+    return HELLO, NAME;
+}
+
+console.log(towParameter('Kudus fatihi', 'Salahedden Eyyubi'))
+
+// Arrow functions example wihtout argument or parameter
 
 let Mevlam = () => {
 console.log('Ibrahimim nar et yerim')
