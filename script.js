bf99a2643b21bd485614e25b82ac0b649b94f548
Zainab <zainabturkmen247@gmail.com>
2023-12-24 17:02:47 +0430

    init

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ let amIhoisting = 'I am not hoisting because i am a variable and variables never
 console.log(amIhoisting)  
     
 //hoisting state   
-
+ 
 // console.log(amIhoisting)  
 // let amIhoisting = 'I am not hoisting because i am a variable and variables never be hoisted';
      
@@ -107,7 +107,7 @@ function iAmHoisting(){
     var hoist = 'hey i am hoisting because i am function'
     console.log(hoist)    
 }  
- 
+
 
 
 
