96f01591841a58cbdabd6a308804e69877194fbb
Zainab <zainabturkmen247@gmail.com>
2023-12-29 19:39:40 +0430

    init

--- script.js
+++ script.js
@@ -138,6 +138,16 @@ console.log(ai)
 const abcde = ["a","b","c","d","e"]
 console.log(abcde[2])
 
+const num = [
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14,15]
+]
+
+console.log(num[0][3])
+console.log(num[1][2])
+console.log(num[2][0])
+
 
 
 
