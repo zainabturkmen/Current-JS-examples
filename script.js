5de18b8a13a2acf08514c5dc35cd9e147ef0b275
Zainab <zainabturkmen247@gmail.com>
2024-01-01 14:42:35 +0430

    init

--- script.js
+++ script.js
@@ -251,6 +251,16 @@ btn.forEach(button => {
         console.log('click hehehe')
     })
 })
+btn.forEach(button => {
+    button.addEventListener('click', ()=> {
+        console.log('click hehehe')
+    })
+})
+btn.forEach(button => {
+    button.addEventListener('click', ()=> {
+        console.log('click hehehe')
+    })
+})
 
 
 
