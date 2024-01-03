35bb87d32d9db983a5d7dce2532aa26c376a8512
Zainab <zainabturkmen247@gmail.com>
2024-01-03 11:47:59 +0430

    init

--- script.js
+++ script.js
@@ -255,4 +255,12 @@ btn.forEach(button => {
 const GrandPa = document.getElementById('GrandParent')
  GrandPa.children[0].style.color = "blue"
 
+
+ let isLogIn = false
+
+ if(isLogIn){
+    console.log("i AM LOG In")
+ }else{
+    console.log("please log in")
+ }
  
